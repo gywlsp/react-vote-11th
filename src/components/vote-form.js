720235b215d25b2baa8c42df4f99adf8979a7521
gywls517 <gywls517@naver.com>
2020-04-12 10:20:49 +0900

    [Feature] Add vote-form component

---
+++ src/components/vote-form.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function VoteForm() {
+  return <Wrapper>안녕 나는 투표 폼!</Wrapper>;
+}
+
+const Wrapper = styled.div`
+  background-color: red;
+  font-size: 18px;
+`;
