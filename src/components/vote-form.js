fe2863681998e0cad277c56ea271f544eaa2ecf3
gywls517 <gywls517@naver.com>
2020-04-12 12:43:38 +0900

    [Enhance] Add vote-form design, event-handler

--- src/components/vote-form.js
+++ src/components/vote-form.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styled from "styled-components";
 
 export default function VoteForm() {
+  const [candidates, setCandidates] = useState(null);
+
+  useEffect(() => {
+    getCandidates();
+  }, []);
+
+  const getCandidates = async () => {
+    const data = await axios
+      .get(process.env.API_HOST + "/candidates/")
+      .then((res) => {
+        console.log(res);
+        return res.data;
+      })
+      .catch((err) => console.log(err));
+    setCandidates(data);
+  };
+
   return <Wrapper>안녕 나는 투표 폼!</Wrapper>;
 }
 
 const Wrapper = styled.div`
-  background-color: red;
+  width: 100%;
+  min-height: 30rem;
+  background-color: white;
   font-size: 18px;
+  padding: 3rem 4rem;
 `;
