ea9a44d6f31c3ff5aecbcea6aed4c86e0ce007e0
gywls517 <gywls517@naver.com>
2020-04-12 12:44:16 +0900

    [Enhance] Add index page design, vote-form component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 import LoginForm from "../src/components/login-form";
+import VoteForm from "../src/components/vote-form";
 
 export default function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <Wrapper>
-      리액트 투-표
-      <LoginForm />
+      <Title>리액트 투-표</Title>
+      {!isLoggedIn && <LoginForm {...{ setIsLoggedIn }} />}
+      {isLoggedIn && <VoteForm />}
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-  background-color: blue;
-  height: 100vh;
+  min-height: 100vh;
+  padding: 10rem 40rem;
+  background-color: Azure;
+`;
+
+const Title = styled.h1`
+  font-size: 4rem;
 `;
